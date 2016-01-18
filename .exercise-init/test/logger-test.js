b27d4bcc93221ea39d5a1f1093cf5bbe042779d2
Mike Bland <michael.bland@gsa.gov>
2016-01-18 18:10:47 -0500

    Start new Logger class

---
+++ .exercise-init/test/logger-test.js
@@ -0,0 +1,10 @@
+/* jshint node: true */
+/* jshint mocha: true */
+/* jshint expr: true */
+
+'use strict';
+
+describe('Logger', function() {
+  it('should prefix all messages with the script name', function() {
+  });
+});
