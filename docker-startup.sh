114088a8f609ccbfa7df572c209b4a1ee4039e8e
James C. Scott <jamesc.scott@gsa.gov>
2016-01-22 18:42:19 -0500

    Add better server detection script for docker
    Instead of sleeping, just ping the server then start the server.

--- docker-startup.sh
+++ docker-startup.sh
@@ -1,9 +1,29 @@
 #!/bin/bash
 
 docker build -t unittest-tutorial .
-echo Open your browser to $(docker-machine ip $DOCKER_MACHINE_NAME):4000
+FAIL=0
+TRIES=10
+if [ ! -d ".bundle" ]; then
+	# Will take a long time to install all the dependencies
+	TRIES=30
+fi
 DOCKER_ID=$(docker run -itd -p 4000:4000 -v $(pwd):/unit-testing-node unittest-tutorial)
-echo "Sleeping to let server come up"
-sleep 3
+echo "Waiting for server to come up. Initial run can take some time."
+while true; do
+	curl --output /dev/null --silent --head --fail http://$(docker-machine ip $DOCKER_MACHINE_NAME):4000
+	if [ $? -eq 0 ]; then
+		echo "Server is up"
+		break
+	else
+		FAIL=$[FAIL +1]
+		sleep 5
+	fi
+	if [ $FAIL -eq $TRIES ]; then
+		echo "Server could not be found. Exiting..."
+		docker rm -f $DOCKER_ID
+		exit
+	fi
+done
+echo "Opening browser"
 open "http://$(docker-machine ip $DOCKER_MACHINE_NAME):4000" &
 docker attach $DOCKER_ID
