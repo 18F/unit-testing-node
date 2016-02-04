549b291d336baa58572fc2faafb9465b632c0918
James C. Scott <jamesc.scott@gsa.gov>
2016-02-04 15:58:38 -0500

    Move to using ubuntu

--- docker.sh
+++ docker.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 JEKYLL_LOG=/var/log/unit-testing-node.log
-bundle install --path .bundle
+/bin/bash -c "gem install colorator"
+/bin/bash -c "gem install bundler"
+/bin/bash -c "bundle install --path .bundle"
 ./go serve -H 0.0.0.0 >> $JEKYLL_LOG 2>&1 &
 echo Jekyll logs can be found at $JEKYLL_LOG
 /bin/bash
